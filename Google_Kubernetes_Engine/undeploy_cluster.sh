f5ef37bbd8099c303f4db9b77d01f90cee8778c3
Arthur Diniz <arthurbdiniz@gmail.com>
2019-04-29 23:36:31 -0300

    Update undeploy script from GKE
    Signed-off-by: Arthur Diniz <arthurbdiniz@gmail.com>

--- Google_Kubernetes_Engine/undeploy_cluster.sh
+++ Google_Kubernetes_Engine/undeploy_cluster.sh
@@ -1,8 +1,3 @@
 #!/usr/bin/env bash
 
-kubectl delete -f ingress/echo_ingress.yaml
-
-kubectl delete -f deployments/echo1.yaml
-kubectl delete -f deployments/echo2.yaml
-
 gcloud container clusters delete kube-cluster
