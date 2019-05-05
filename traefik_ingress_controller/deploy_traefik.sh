b6468b70bdbd85a400bcc016a128dbb40abfee8d
Arthur Diniz <arthurbdiniz@gmail.com>
2019-05-05 03:09:51 -0300

    Fix traefik tutorial for GKE
    Signed-off-by: Arthur Diniz <arthurbdiniz@gmail.com>

--- traefik_ingress_controller/deploy_traefik.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-kubectl apply -f 01_permissions.yml
-kubectl apply -f 02_cluster-role.yml
-kubectl apply -f 03_config.yml
-kubectl apply -f 04_deployment.yml
-kubectl apply -f 05_service.yml
